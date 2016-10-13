577a34f003792ed499fe1e8acc4bd1dc588c7cc4
Madison Scott-Clary <madison.scott-clary@canonical.com>
2016-10-12 20:25:21 -0600

    Further styling

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# honeycomb
-Honeycomb is a new site for authors and readers
+&#11041; Honeycomb
+==================
+
+&#11041; Honeycomb is a site designed specifically for the written word, aiming to
+provide the best experience for both authors and readers.
---
+++ usermgmt/templatetags/form_extras.py
@@ -0,0 +1,9 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+@register.filter(is_safe=True)
+@stringfilter
+def append_form_control(value):
+    return value.replace('input', 'input class="form-control"')
