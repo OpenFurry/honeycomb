7d5c43e0c811bc45daee6e478e67e3c33b497033
Madison Scott-Clary <madison.scott-clary@canonical.com>
2016-10-22 15:50:42 -0600

    Fix git-revno to always run in one dir

--- core/templatetags/git_revno.py
+++ core/templatetags/git_revno.py
@@ -1,10 +1,17 @@
-from subprocess import check_output
+from subprocess import (
+    PIPE,
+    Popen,
+)
 
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
 
 @register.simple_tag
 def git_revno():
-    return check_output(['git', 'rev-parse', '--verify', 'HEAD']).strip()[-7:]
+    p = Popen(['git', 'rev-parse', '--verify', 'HEAD'], stdout=PIPE,
+              cwd=settings.BASE_DIR)
+    out, _ = p.communicate()
+    return out.strip()[-7:]
