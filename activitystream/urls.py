9b38a4e6fd3cc1a6701bb65bf99045a3c6b66ebb
Madison Scott-Clary <madison.scott-clary@canonical.com>
2016-10-30 14:05:11 -0600

    Trivial, forgot a few things

--- activitystream/urls.py
+++ activitystream/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url('^$', views.sitewide_data, name='sitewide_data'),
     url('^stream/$', views.get_stream, name='get_stream'),
     url('^stream/(?P<models>[\w_:]+)/$', views.get_stream, name='get_stream'),
-    url('^stream/(?P<models>[\w_:]+)/(?P<object_id>\d+)',
+    url('^stream/(?P<models>[\w_:]+)/(?P<object_id>\d+)/$',
         views.get_stream, name='get_stream'),
 ]
